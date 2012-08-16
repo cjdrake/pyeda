2f2be171d8ac44563fa67de6b75835dc7db6698b
Chris Drake <cjdrake@gmail.com>
2012-08-15 23:28:28 -0700

    Move all table stuff to booltable module

--- pyeda/boolexpr.py
+++ pyeda/boolexpr.py
@@ -53,15 +53,6 @@ NUMBERS = dict()
 VARIABLES = dict()
 COMPLEMENTS = dict()
 
-# Positional Cube Notation
-#PC_VOID, PC_ONE, PC_ZERO, PC_DC = range(4)
-
-#PC_STR = {
-#    PC_ZERO : "0",
-#    PC_ONE  : "1",
-#    PC_DC   : "*"
-#}
-
 def num(x):
     """Return a unique Boolean number."""
     n = int(x)
@@ -226,46 +217,6 @@ def int2vec(n, length=None):
     return vv
 
 
-#class Table(Scalar):
-#    pass
-#
-#
-#class TruthTable(Table):
-#
-#    def __init__(self, inputs, outputs):
-#        self._inputs = inputs
-#        self._data = bytearray()
-#        self._length = 0
-#        pos = 0
-#        for pc_val in outputs:
-#            if pos == 0:
-#                self._data.append(0)
-#            self._data[-1] += pc_val << pos
-#            self._length += 1
-#            pos = (pos + 2) & 0x07
-#        assert self._length == 2 ** len(self._inputs)
-#
-#    def __len__(self):
-#        return self._length
-#
-#    def __str__(self):
-#        s = ["f(" + ", ".join(str(v) for v in self._inputs) + ") = "]
-#        for i in range(self._length):
-#            pos = (i & 0x03) << 1
-#            byte = self._data[(i >> 2)] >> pos
-#            pc_val = byte & 0x03
-#            s.append(PC_STR[pc_val])
-#        return "".join(s)
-#
-#    @property
-#    def support(self):
-#        return set(self._inputs)
-#
-#    @property
-#    def inputs(self):
-#        return self._inputs
-
-
 class Expression(Function):
     """Boolean function represented by a logic expression"""
 
@@ -318,10 +269,6 @@ class Expression(Function):
         for n in range(2 ** abs(self)):
             d = {v: _bit_on(n, i) for i, v in enumerate(self.inputs)}
             yield self.restrict(d)
-
-    #def to_truth_table(self):
-    #    outputs = ((PC_ONE if x else PC_ZERO) for x in self.iter_outputs())
-    #    return TruthTable(self.inputs, outputs)
 
     def factor(self):
         """Return a factored expression.
---
+++ pyeda/booltable.py
@@ -0,0 +1,51 @@
+"""
+Boolean Tables
+"""
+
+# Positional Cube Notation
+#PC_VOID, PC_ONE, PC_ZERO, PC_DC = range(4)
+
+#PC_STR = {
+#    PC_ZERO : "0",
+#    PC_ONE  : "1",
+#    PC_DC   : "*"
+#}
+
+#class Table(Scalar):
+#    pass
+#
+#
+#class TruthTable(Table):
+#
+#    def __init__(self, inputs, outputs):
+#        self._inputs = inputs
+#        self._data = bytearray()
+#        self._length = 0
+#        pos = 0
+#        for pc_val in outputs:
+#            if pos == 0:
+#                self._data.append(0)
+#            self._data[-1] += pc_val << pos
+#            self._length += 1
+#            pos = (pos + 2) & 0x07
+#        assert self._length == 2 ** len(self._inputs)
+#
+#    def __len__(self):
+#        return self._length
+#
+#    def __str__(self):
+#        s = ["f(" + ", ".join(str(v) for v in self._inputs) + ") = "]
+#        for i in range(self._length):
+#            pos = (i & 0x03) << 1
+#            byte = self._data[(i >> 2)] >> pos
+#            pc_val = byte & 0x03
+#            s.append(PC_STR[pc_val])
+#        return "".join(s)
+#
+#    @property
+#    def support(self):
+#        return set(self._inputs)
+#
+#    @property
+#    def inputs(self):
+#        return self._inputs
