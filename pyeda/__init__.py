eae17dd41c9c1f2da2e10a1875f402a8d796e616
Chris Drake <cjdrake@gmail.com>
2012-08-23 02:52:14 -0700

    Update to version 0.3.0

--- pyeda/__init__.py
+++ pyeda/__init__.py
@@ -1,10 +1,11 @@
 """
 Python EDA Package
 
-common.py -- Convenient place to put common functions
-boolfunc.py -- Boolean Functions
-expr.py -- Boolean Expressions
+common.py
+boolfunc.py -- Boolean functions
+expr.py -- Boolean logic expressions
+vexpr.py -- Boolean vector logic expressions
 """
 
 __copyright__ = "Copyright (c) 2012, Chris Drake"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
