8d85f8b9a9f461f47e07cd0ba54d998b109f556c
Chris Drake <cjdrake@gmail.com>
2012-09-26 21:28:52 -0700

    Add classifiers to setup file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __license__ = "All rights reserved."
 from distutils.core import setup
 
 # pyeda
-from pyeda import __version__
+import pyeda
 
 with open("README.rst") as fin:
     README = fin.read()
@@ -17,14 +17,26 @@ with open("LICENSE") as fin:
 
 PACKAGES = ["pyeda"]
 
+CLASSIFIERS = [
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.2",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Mathematics",
+]
+
 setup(
     name="pyeda",
-    version=__version__,
+    version=pyeda.__version__,
     description="Python Electronic Design Automation",
     long_description=README,
     author="Chris Drake",
     author_email="cjdrake AT gmail DOT com",
     url="https://github.com/cjdrake/pyeda",
     license=LICENSE,
-    packages=PACKAGES
+    packages=PACKAGES,
+    classifiers=CLASSIFIERS
 )
