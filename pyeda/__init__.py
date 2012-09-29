db3f6fc5c8287cb603c4bb767a7be8ea54606419
Chris Drake <cjdrake@gmail.com>
2012-09-29 16:13:31 -0700

    Change cnf module to nfexpr (normal form expr)

--- pyeda/__init__.py
+++ pyeda/__init__.py
@@ -14,10 +14,10 @@ vexpr.py    -- Boolean vector logic expressions
 __copyright__ = "Copyright (c) 2012, Chris Drake"
 __version__ = "0.7.0"
 
-from pyeda.cnf import expr2dnf, expr2cnf, dnf2expr, cnf2expr, DNF_Or, CNF_And
 from pyeda.constant import ZERO, ONE
 from pyeda.expr import var
 from pyeda.expr import Nor, Nand, OneHot0, OneHot
 from pyeda.expr import Not, Or, And, Xor, Xnor, Implies, Equal
+from pyeda.nfexpr import expr2dnf, expr2cnf, dnf2expr, cnf2expr, DNF_Or, CNF_And
 from pyeda.table import TruthTable, expr2truthtable
 from pyeda.vexpr import bitvec, sbitvec, uint2vec, int2vec
