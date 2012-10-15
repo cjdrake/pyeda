8cbea4dce3a3b55e9af70fdd6c77975db7802fde
Chris Drake <cjdrake@gmail.com>
2012-10-14 22:57:57 -0700

    Initial version of dimacs CNF parser

--- pyeda/dimacs.py
+++ pyeda/dimacs.py
@@ -2,24 +2,24 @@
 Dimacs
 """
 
-from expr import var, Not, Or, And, Xor, Equal
-from vexpr import bitvec
+from pyeda.expr import var, Not, Or, And, Xor, Equal
+from pyeda.vexpr import bitvec
 
 PREAMBLE, CNF, SAT = range(3)
 
 def parse(stream):
     state = PREAMBLE
-    args = list()
+    clauses = [[]]
     for i, line in enumerate(stream.splitlines()):
         if state == PREAMBLE:
             if line.startswith('c'):
                 pass
             elif line.startswith('p'):
                 sp = line.split()
-                if len(sp) == 2:
-                    fmt, nvars, nargs = sp[0], sp[1], None
-                elif len(sp) == 3:
-                    fmt, nvars, nargs = sp
+                if len(sp) == 3:
+                    fmt, nvars, nargs = sp[1], sp[2], None
+                elif len(sp) == 4:
+                    fmt, nvars, nargs = sp[1], sp[2], sp[3]
                 else:
                     raise SyntaxError("line {}: {}".format(i, line))
                 try:
@@ -37,14 +37,19 @@ def parse(stream):
                     raise SyntaxError("invalid FORMAT: " + fmt)
         elif state == CNF:
             try:
-                args += [int(n) for n in line.split()]
+                nums = [int(n) for n in line.split()]
             except ValueError:
                 raise SyntaxError("line {}: {}".format(i, line))
+            for num in nums:
+                if num == 0:
+                    clauses.append([])
+                else:
+                    try:
+                        clauses[-1].append(-X[-num] if num < 0 else X[num])
+                    except IndexError:
+                        raise ValueError("invalid number: " + str(num))
         elif state == SAT:
             raise NotImplementedError("SAT format not supported yet")
 
-#def emit(expr):
-#    pass
-
-# load, dump
-# file input or string input?
+    # Assume CNF
+    return And(*[Or(*clause) for clause in clauses])
