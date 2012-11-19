16bd9bf93370e0dd08566cf0af973d858bad3cff
Chris Drake <cjdrake@gmail.com>
2012-11-18 22:38:01 -0800

    Change Sudoku example to 1-based index

--- example/sudoku.py
+++ example/sudoku.py
@@ -35,62 +35,62 @@ from pyeda import *
 
 DIGITS = "123456789"
 
-X = bitvec("x", 9, 9, 9)
+X = bitvec("x", (1, 10), (1, 10), (1, 10))
 
 V = And(*[
         And(*[
             OneHot(*[ X[r][c][v]
-                for v in range(3 * 3) ])
-            for c in range(3 * 3) ])
-        for r in range(3 * 3) ])
+                for v in range(1, 10) ])
+            for c in range(1, 10) ])
+        for r in range(1, 10) ])
 
 R = And(*[
         And(*[
             OneHot(*[ X[r][c][v]
-                for c in range(3 * 3) ])
-            for v in range(3 * 3) ])
-        for r in range(3 * 3) ])
+                for c in range(1, 10) ])
+            for v in range(1, 10) ])
+        for r in range(1, 10) ])
 
 C = And(*[
         And(*[
             OneHot(*[ X[r][c][v]
-                for r in range(3 * 3) ])
-            for v in range(3 * 3) ])
-        for c in range(3 * 3) ])
+                for r in range(1, 10) ])
+            for v in range(1, 10) ])
+        for c in range(1, 10) ])
 
 B = And(*[
         And(*[
             OneHot(*[ X[3*br+r][3*bc+c][v]
-                for r in range(3) for c in range(3) ])
-            for v in range(3 * 3) ])
+                for r in range(1, 4) for c in range(1, 4) ])
+            for v in range(1, 10) ])
         for br in range(3) for bc in range(3) ])
 
 S = CNF_And(V, R, C, B)
 
 def parse_grid(grid):
     chars = [c for c in grid if c in DIGITS or c in "0."]
-    assert len(chars) == (3 * 3) ** 2
-    I = And(*[ X[i // (3 * 3)][i % (3 * 3)][int(c) - 1]
+    assert len(chars) == 9 ** 2
+    I = And(*[ X[i // 9 + 1][i % 9 + 1][int(c)]
                for i, c in enumerate(chars) if c in DIGITS ])
     return expr2cnf(I)
 
 def get_val(point, r, c):
-    for v in range(3 * 3):
+    for v in range(1, 10):
         if point[X[r][c][v]]:
-            return DIGITS[v]
+            return DIGITS[v-1]
     return "X"
 
 def display(point):
     chars = list()
-    for r in range(3 * 3):
-        for c in range(3 * 3):
-            if c != 0 and c % 3 == 0:
+    for r in range(1, 10):
+        for c in range(1, 10):
+            if c in (4, 7):
                 chars.append("|")
             chars.append(get_val(point, r, c))
-        if r != (3 * 3 - 1):
+        if r != 9:
             chars.append("\n")
-            if r % 3 == (3 - 1):
-                chars.append("+".join(["-" * 3] * 3) + "\n")
+            if r in (3, 6):
+                chars.append("---+---+---\n")
     print("".join(chars))
 
 def solve(grid):
