7e4a33e6e1ff25ca623d507c50e4b090a58e93df
Chris Drake <cjdrake@gmail.com>
2013-03-11 00:19:35 -0700

    Move gray code tests into test module

--- pyeda/arithmetic.py
+++ pyeda/arithmetic.py
@@ -11,9 +11,7 @@ from pyeda.expr import Xor
 from pyeda.vexpr import BitVector
 
 def ripple_carry_add(A, B, cin=0):
-    """Return symbolic logic for an N-bit ripple carry adder.
-
-    """
+    """Return symbolic logic for an N-bit ripple carry adder."""
     assert len(A) == len(B)
     s, c = list(), list()
     for i, ai in enumerate(A, A.start):
@@ -23,9 +21,7 @@ def ripple_carry_add(A, B, cin=0):
     return BitVector(s), BitVector(c)
 
 def kogge_stone_add(A, B, cin=0):
-    """Return symbolic logic for an N-bit Kogge-Stone adder.
-
-    """
+    """Return symbolic logic for an N-bit Kogge-Stone adder."""
     assert len(A) == len(B)
     stop = len(A)
     # generate/propagate logic
@@ -41,29 +37,12 @@ def kogge_stone_add(A, B, cin=0):
     return BitVector(s), BitVector(g)
 
 def bin2gray(B):
-    """Convert a binary-coded vector into a gray-coded vector.
-
-    >>> from pyeda import bitvec, uint2vec
-    >>> B = bitvec("B", 3)
-    >>> G = bin2gray(B)
-    >>> gnums = [G.vrestrict({B: uint2vec(i, 3)}).to_uint() for i in range(8)]
-    >>> print(gnums)
-    [0, 1, 3, 2, 6, 7, 5, 4]
-    """
+    """Convert a binary-coded vector into a gray-coded vector."""
     items = [Xor(B[i], B[i+1]) for i in range(B.start, B.stop-1)]
     items.append(B[B.stop-1])
     return BitVector(items, B.start)
 
 def gray2bin(G):
-    """Convert a gray-coded vector into a binary-coded vector.
-
-    >>> from pyeda import bitvec, uint2vec
-    >>> G = bitvec("G", 3)
-    >>> B = gray2bin(G)
-    >>> gnums = [0, 1, 3, 2, 6, 7, 5, 4]
-    >>> bnums = [B.vrestrict({G: uint2vec(i, 3)}).to_uint() for i in gnums]
-    >>> print(bnums)
-    [0, 1, 2, 3, 4, 5, 6, 7]
-    """
+    """Convert a gray-coded vector into a binary-coded vector."""
     items = [G[i:].uxor() for i, _ in enumerate(G, G.start)]
     return BitVector(items, G.start)
