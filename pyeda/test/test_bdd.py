099ae768056a4ab160179be89c8750a2bfc06b2c
Chris Drake <cjdrake@gmail.com>
2013-04-21 00:46:29 -0700

    Implement expr2bdd function and unique table

--- pyeda/test/test_bdd.py
+++ pyeda/test/test_bdd.py
@@ -1,3 +1,21 @@
 """
 Test binary decision diagrams
 """
+
+from pyeda.bdd import expr2bdd
+from pyeda.expr import var
+
+a, b, c = map(var, 'abc')
+
+def test_expr2bdd():
+    f = a * b + a * c + b * c
+    bdd_f = expr2bdd(f)
+    assert bdd_f.root == a.var
+    assert bdd_f.low.root == b.var
+    assert bdd_f.high.root == b.var
+    assert bdd_f.low.low.root == 0
+    assert bdd_f.low.high.root == c.var
+    assert bdd_f.high.low.root == c.var
+    assert bdd_f.high.high.root == 1
+    assert bdd_f.low.high.low.root == 0
+    assert bdd_f.high.low.high.root == 1
