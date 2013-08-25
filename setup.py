9e6d3eda5485487b868234f27426a5797adfafd6
Chris Drake <cjdrake@gmail.com>
2013-08-24 17:54:44 -0700

    Remove Python 2.7 from package metadata

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ CLASSIFIERS = [
     "License :: OSI Approved :: BSD License",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.2",
     "Programming Language :: Python :: 3.3",
