25580f4b0f4588be9716f7b0aee8610d70949993
Chris Drake <cjdrake@gmail.com>
2013-09-21 21:06:54 -0700

    Reorganize pyeda package

--- pyeda/__init__.py
+++ pyeda/__init__.py
@@ -10,13 +10,15 @@ PC_VOID, PC_ONE, PC_ZERO, PC_DC = range(4)
 import pyeda.alphas
 import pyeda.arithmetic
 
+from pyeda.util import clog2, boolify
+
 from pyeda.boolfunc import (
     num2point, num2upoint, num2term, point2upoint, point2term,
-    iter_points, iter_upoints, iter_terms
+    iter_points, iter_upoints, iter_terms,
 )
 from pyeda.bdd import (
     bddvar, expr2bdd, bdd2expr, upoint2bddpoint,
-    BinaryDecisionDiagram, BDDConstant, BDDVariable
+    BinaryDecisionDiagram, BDDConstant, BDDVariable,
 )
 from pyeda.expr import (
     exprvar, exprcomp, upoint2exprpoint,
@@ -24,17 +26,18 @@ from pyeda.expr import (
     Nor, Nand, OneHot0, OneHot,
 )
 from pyeda.vexpr import (
-    bitvec, uint2vec, int2vec, BitVector
+    bitvec, uint2vec, int2vec, BitVector,
 )
 from pyeda.nfexpr import (
     expr2dnf, expr2cnf, nf2expr, upoint2nfpoint,
     NF_Not, CNF_And, DNF_Or,
-    NormalForm, DisjNormalForm, ConjNormalForm
+    NormalForm, DisjNormalForm, ConjNormalForm,
 )
 from pyeda.table import (
     ttvar, truthtable, expr2truthtable, truthtable2expr,
-    TruthTable, TTConstant, TTVariable
+    TruthTable, TTConstant, TTVariable,
 )
-from pyeda.util import clog2, boolify
 
-from pyeda.parsing.dimacs import load_cnf, dump_cnf, load_sat, dump_sat
+from pyeda.parsing.dimacs import (
+    load_cnf, dump_cnf, load_sat, dump_sat,
+)
