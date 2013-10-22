1e027cc5234bfdf8ea4fb3dca7c466fd30c439f7
Chris Drake <cjdrake@gmail.com>
2013-10-21 19:18:17 -0700

    Add Brent-Kung adder logic

--- pyeda/logic/addition.py
+++ pyeda/logic/addition.py
@@ -4,10 +4,13 @@ Logic functions for addition
 Interface Functions:
     ripple_carry_add
     kogge_stone_add
+    brent_kung_add
 """
 
 # Disable "invalid variable name"
 # pylint: disable=C0103
+
+from math import floor, log2
 
 from pyeda.boolalg.expr import Xor, Majority
 from pyeda.boolalg.vexpr import BitVector
@@ -37,4 +40,29 @@ def kogge_stone_add(A, B, cin=0):
             p[j] = p[j] * p[j-start]
     # sum logic
     s = [Xor(A[i], B[i], (cin if i == 0 else g[i-1])) for i in range(stop)]
+    return BitVector(s), BitVector(g)
+
+def brent_kung_add(A, B, cin=0):
+    """Return symbolic logic for an N-bit Brent-Kung adder."""
+    assert len(A) == len(B)
+    N = len(A)
+    # generate/propagate logic
+    g = [A[i] * B[i] for i in range(N)]
+    p = [Xor(A[i], B[i]) for i in range(N)]
+    # carry tree
+    for i in range(floor(log2(N))):
+        step = 2**i
+        for start in range(2**(i+1)-1, N, 2**(i+1)):
+            g[start] = g[start] + p[start] * g[start-step]
+            p[start] = p[start] * p[start-step]
+    # inverse carry tree
+    for i in range(floor(log2(N))-2, -1, -1):
+        start = 2**(i+1)-1
+        step = 2**i
+        while start + step < N:
+            g[start+step] = g[start+step] + p[start+step] * g[start]
+            p[start+step] = p[start+step] * p[start]
+            start += step
+    # sum logic
+    s = [Xor(A[i], B[i], (cin if i == 0 else g[i-1])) for i in range(N)]
     return BitVector(s), BitVector(g)
--- pyeda/logic/test/test_addition.py
+++ pyeda/logic/test/test_addition.py
@@ -4,8 +4,11 @@ Test logic functions for addition
 
 import random
 
-from pyeda.logic.addition import ripple_carry_add as rca
-from pyeda.logic.addition import kogge_stone_add as ksa
+from pyeda.logic.addition import (
+    ripple_carry_add as rca,
+    kogge_stone_add as ksa,
+    brent_kung_add as bka,
+)
 from pyeda.boolalg.vexpr import bitvec, uint2vec, int2vec
 
 NVECS = 100
@@ -21,12 +24,12 @@ def sadd(S, A, B, aval, bval):
     return R.to_int()
 
 def test_unsigned_add():
-    N = 8
+    N = 9
 
     A = bitvec('A', N)
     B = bitvec('B', N)
 
-    for adder in (rca, ksa):
+    for adder in (rca, ksa, bka):
         S, C = adder(A, B)
         S.append(C[N-1])
 
@@ -47,7 +50,7 @@ def test_signed_add():
     A = bitvec('A', 8)
     B = bitvec('B', 8)
 
-    for adder in (rca, ksa):
+    for adder in (rca, ksa, bka):
         S, C = adder(A, B)
 
         # 0 + 0 = 0
