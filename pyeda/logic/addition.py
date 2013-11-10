5cfd218f55b09294066739ef705a55c1e44ee43b
Chris Drake <cjdrake@gmail.com>
2013-11-09 19:10:31 -0800

    Fix issue with Python 3.2

--- pyeda/logic/addition.py
+++ pyeda/logic/addition.py
@@ -10,7 +10,7 @@ Interface Functions:
 # Disable "invalid variable name"
 # pylint: disable=C0103
 
-from math import floor, log2
+from math import floor, log
 
 from pyeda.boolalg.expr import Xor, Majority
 from pyeda.boolalg.vexpr import BitVector
@@ -50,13 +50,13 @@ def brent_kung_add(A, B, cin=0):
     g = [A[i] * B[i] for i in range(N)]
     p = [Xor(A[i], B[i]) for i in range(N)]
     # carry tree
-    for i in range(floor(log2(N))):
+    for i in range(floor(log(N, 2))):
         step = 2**i
         for start in range(2**(i+1)-1, N, 2**(i+1)):
             g[start] = g[start] + p[start] * g[start-step]
             p[start] = p[start] * p[start-step]
     # inverse carry tree
-    for i in range(floor(log2(N))-2, -1, -1):
+    for i in range(floor(log(N, 2))-2, -1, -1):
         start = 2**(i+1)-1
         step = 2**i
         while start + step < N:
