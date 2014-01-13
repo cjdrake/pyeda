cbf69cc672be359a6a36d17c848d1d2d93b81933
Chris Drake <cjdrake@gmail.com>
2014-01-12 17:39:22 -0800

    Use Python-3.3+

--- setup.py
+++ setup.py
@@ -55,8 +55,8 @@ CLASSIFIERS = [
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.2",
     "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
     "Topic :: Scientific/Engineering",
     "Topic :: Scientific/Engineering :: Mathematics",
 ]
