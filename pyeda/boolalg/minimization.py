2b2a48cffe6040e2f06e64d6929d868b8cc77e75
Chris Drake <cjdrake@gmail.com>
2014-01-14 20:31:41 -0800

    Add pyeda-espresso interface module

---
+++ pyeda/boolalg/minimization.py
@@ -0,0 +1,67 @@
+"""
+Boolean Minimization
+
+Interface Functions:
+    espresso_exprs
+"""
+
+from pyeda.boolalg.espresso import (
+    FTYPE, DTYPE, RTYPE,
+    espresso,
+)
+from pyeda.boolalg.expr import Expression, Or, And
+
+def espresso_exprs(exprs):
+    """Return a list of expressions optimized using Espresso."""
+    support = frozenset.union(*[f.support for f in exprs])
+    inputs = sorted(support)
+
+    # Gather all cubes in the cover of input functions
+    fscover = set()
+    for f in exprs:
+        if not (isinstance(f, Expression) and f.is_dnf()):
+            raise ValueError("expected a DNF expression, got " + str(f))
+        fscover |= f.cover
+
+    num_inputs = len(inputs)
+    num_outputs = len(exprs)
+
+    implicants = set()
+    for fscube in fscover:
+        invec = list()
+        for v in inputs:
+            if ~v in fscube:
+                invec.append(1)
+            elif v in fscube:
+                invec.append(2)
+            else:
+                invec.append(3)
+        outvec = list()
+        for f in exprs:
+            for fcube in f.cover:
+                if fcube <= fscube:
+                    outvec.append(1)
+                    break
+            else:
+                outvec.append(0)
+
+        implicants.add((tuple(invec), tuple(outvec)))
+
+    implicants = espresso(num_inputs, num_outputs, implicants, intype=FTYPE)
+
+    fs = list()
+    for i in range(num_outputs):
+        terms = list()
+        for invec, outvec in implicants:
+            if outvec[i]:
+                term = list()
+                for j, v in enumerate(inputs):
+                    if invec[j] == 1:
+                        term.append(~v)
+                    elif invec[j] == 2:
+                        term.append(v)
+                terms.append(term)
+        fs.append(Or(*[And(*term) for term in terms]))
+
+    return fs
+
