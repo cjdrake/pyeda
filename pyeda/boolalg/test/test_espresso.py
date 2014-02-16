e9e65a597e478bcb7580178ba3c79c4636bd071f
Chris Drake <cjdrake@gmail.com>
2014-02-15 21:25:02 -0800

    Add BOOM benchmark Espresso tests

--- pyeda/boolalg/test/test_espresso.py
+++ pyeda/boolalg/test/test_espresso.py
@@ -2,13 +2,28 @@
 Test the Espresso interface
 """
 
+import os
+from multiprocessing import Pool
+
 from pyeda.boolalg import espresso
 from pyeda.boolalg.table import truthtable2expr, truthtable
 from pyeda.boolalg.vexpr import bitvec
 from pyeda.boolalg.minimization import espresso_exprs, espresso_tts
 from pyeda.logic.addition import ripple_carry_add
+from pyeda.parsing.pla import parse_pla
 
 from nose.tools import assert_raises
+
+BOOM_PLAS = [
+    'bb_50x5x50_20%_0.pla',
+    'bb_50x5x50_20%_1.pla',
+    'bb_50x5x50_20%_2.pla',
+    'bb_50x5x50_20%_3.pla',
+    'bb_50x5x50_20%_4.pla',
+    'bb_50x5x50_20%_5.pla',
+    'bb_50x5x50_20%_6.pla',
+    'bb_50x5x50_20%_7.pla',
+]
 
 def test_espresso():
     assert espresso.FTYPE == 1
@@ -20,7 +35,6 @@ def test_espresso():
     S, C = ripple_carry_add(A, B)
     s0, s1, s2, s3 = espresso_exprs(S[0].to_dnf(), S[1].to_dnf(),
                                     S[2].to_dnf(), S[3].to_dnf())
-
     assert s0.equivalent(S[0])
     assert s1.equivalent(S[1])
     assert s2.equivalent(S[2])
@@ -32,6 +46,17 @@ def test_espresso():
     f1m, f2m = espresso_tts(f1, f2)
     truthtable2expr(f1).equivalent(f1m)
     truthtable2expr(f2).equivalent(f2m)
+
+def _do_espresso(fname):
+    fpath = os.path.join('extension', 'espresso', 'test', 'bb_all', fname)
+    with open(fpath) as fin:
+        pla = parse_pla(fin)
+    return espresso.espresso(pla['ninputs'], pla['noutputs'], pla['cover'], intype=pla['intype'])
+
+def test_boom():
+    p = Pool(4)
+    # Espresso has an internal 'verify' function
+    p.map(_do_espresso, BOOM_PLAS)
 
 def test_errors():
     assert_raises(ValueError, espresso_exprs, "bad input")
