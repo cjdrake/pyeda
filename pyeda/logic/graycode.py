241592c6c5dcee3d88c0d3154d48526fb15d98b3
Chris Drake <cjdrake@gmail.com>
2014-03-15 21:13:06 -0700

    Convert graycode logic to use farray

--- pyeda/logic/graycode.py
+++ pyeda/logic/graycode.py
@@ -9,17 +9,13 @@ Interface Functions:
 # Disable "invalid variable name"
 # pylint: disable=C0103
 
-from pyeda.boolalg.expr import Xor
-from pyeda.boolalg.vexpr import BitVector
+from pyeda.boolalg.bfarray import cat, farray
 
 def bin2gray(B):
     """Convert a binary-coded vector into a gray-coded vector."""
-    items = [Xor(B[i], B[i+1]) for i in range(B.start, B.stop-1)]
-    items.append(B[B.stop-1])
-    return BitVector(items, B.start)
+    return cat(B[:-1] ^ B[1:], B[-1])
 
 def gray2bin(G):
     """Convert a gray-coded vector into a binary-coded vector."""
-    items = [G[i:].uxor() for i, _ in enumerate(G, G.start)]
-    return BitVector(items, G.start)
+    return farray([G[i:].uxor() for i, _ in enumerate(G)])
 
