fe3b3f5f9d080df195a5bedad6bda3acc4ad9565
Chris Drake <cjdrake@gmail.com>
2014-03-30 22:29:01 -0700

    Minor documentation improvements

--- pyeda/boolalg/test/test_vexpr.py
+++ pyeda/boolalg/test/test_vexpr.py
@@ -22,24 +22,24 @@ def test_bitvec():
 def test_uint2exprs():
     assert_raises(ValueError, uint2exprs, -1)
     assert_raises(ValueError, uint2exprs, 42, 4)
-    assert str(uint2exprs(0)) == "[0]"
-    assert str(uint2exprs(1)) == "[1]"
-    assert str(uint2exprs(2)) == "[0, 1]"
-    assert str(uint2exprs(3)) == "[1, 1]"
-    assert str(uint2exprs(4)) == "[0, 0, 1]"
-    assert str(uint2exprs(4, 4)) == "[0, 0, 1, 0]"
+    assert str(uint2exprs(0)) == "farray([0])"
+    assert str(uint2exprs(1)) == "farray([1])"
+    assert str(uint2exprs(2)) == "farray([0, 1])"
+    assert str(uint2exprs(3)) == "farray([1, 1])"
+    assert str(uint2exprs(4)) == "farray([0, 0, 1])"
+    assert str(uint2exprs(4, 4)) == "farray([0, 0, 1, 0])"
 
 def test_int2exprs():
     assert_raises(ValueError, int2exprs, 42, 4)
-    assert str(int2exprs(-4)) == "[0, 0, 1]"
-    assert str(int2exprs(-3)) == "[1, 0, 1]"
-    assert str(int2exprs(-2)) == "[0, 1]"
-    assert str(int2exprs(-1)) == "[1]"
-    assert str(int2exprs(0)) == "[0]"
-    assert str(int2exprs(1)) == "[1, 0]"
-    assert str(int2exprs(2)) == "[0, 1, 0]"
-    assert str(int2exprs(3)) == "[1, 1, 0]"
-    assert str(int2exprs(3, 4)) == "[1, 1, 0, 0]"
+    assert str(int2exprs(-4)) == "farray([0, 0, 1])"
+    assert str(int2exprs(-3)) == "farray([1, 0, 1])"
+    assert str(int2exprs(-2)) == "farray([0, 1])"
+    assert str(int2exprs(-1)) == "farray([1])"
+    assert str(int2exprs(0)) == "farray([0])"
+    assert str(int2exprs(1)) == "farray([1, 0])"
+    assert str(int2exprs(2)) == "farray([0, 1, 0])"
+    assert str(int2exprs(3)) == "farray([1, 1, 0])"
+    assert str(int2exprs(3, 4)) == "farray([1, 1, 0, 0])"
 
 def test_ops():
     X = bitvec('x', 4)
@@ -52,10 +52,10 @@ def test_ops():
     assert X.uxor().equivalent(X[3] ^ X[2] ^ X[1] ^ X[0])
     assert X.uxnor().equivalent(~(X[3] ^ X[2] ^ X[1] ^ X[0]))
 
-    assert str(~X) == "[~x[0], ~x[1], ~x[2], ~x[3]]"
-    assert str(X | Y) == "[Or(x[0], y[0]), Or(x[1], y[1]), Or(x[2], y[2]), Or(x[3], y[3])]"
-    assert str(X & Y) == "[And(x[0], y[0]), And(x[1], y[1]), And(x[2], y[2]), And(x[3], y[3])]"
-    assert str(X ^ Y) == "[Xor(x[0], y[0]), Xor(x[1], y[1]), Xor(x[2], y[2]), Xor(x[3], y[3])]"
+    assert str(~X) == "farray([~x[0], ~x[1], ~x[2], ~x[3]])"
+    assert str(X | Y) == "farray([Or(x[0], y[0]), Or(x[1], y[1]), Or(x[2], y[2]), Or(x[3], y[3])])"
+    assert str(X & Y) == "farray([And(x[0], y[0]), And(x[1], y[1]), And(x[2], y[2]), And(x[3], y[3])])"
+    assert str(X ^ Y) == "farray([Xor(x[0], y[0]), Xor(x[1], y[1]), Xor(x[2], y[2]), Xor(x[3], y[3])])"
 
 def test_shift():
     X = bitvec('x', 8)
@@ -66,43 +66,43 @@ def test_shift():
     assert_raises(ValueError, X.lsh, 9)
     assert_raises(ValueError, X.lsh, 2, Y)
     a, b = X.lsh(4)
-    assert str(a) == "[0, 0, 0, 0, x[0], x[1], x[2], x[3]]"
-    assert str(b) == "[x[4], x[5], x[6], x[7]]"
+    assert str(a) == "farray([0, 0, 0, 0, x[0], x[1], x[2], x[3]])"
+    assert str(b) == "farray([x[4], x[5], x[6], x[7]])"
     a, b = X.lsh(4, Y)
-    assert str(a) == "[y[0], y[1], y[2], y[3], x[0], x[1], x[2], x[3]]"
-    assert str(b) == "[x[4], x[5], x[6], x[7]]"
+    assert str(a) == "farray([y[0], y[1], y[2], y[3], x[0], x[1], x[2], x[3]])"
+    assert str(b) == "farray([x[4], x[5], x[6], x[7]])"
     a, b = X.lsh(0)
-    assert str(a) == "[x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]]"
+    assert str(a) == "farray([x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]])"
     assert not b
     assert_raises(TypeError, X.__lshift__, "foo")
-    assert str(X << 4) == "[0, 0, 0, 0, x[0], x[1], x[2], x[3]]"
-    assert str(X << (4, Y)) == "[y[0], y[1], y[2], y[3], x[0], x[1], x[2], x[3]]"
+    assert str(X << 4) == "farray([0, 0, 0, 0, x[0], x[1], x[2], x[3]])"
+    assert str(X << (4, Y)) == "farray([y[0], y[1], y[2], y[3], x[0], x[1], x[2], x[3]])"
 
     # right shift
     assert_raises(ValueError, X.rsh, -1)
     assert_raises(ValueError, X.rsh, 9)
     assert_raises(ValueError, X.rsh, 2, Y)
     a, b = X.rsh(4)
-    assert str(a) == "[x[4], x[5], x[6], x[7], 0, 0, 0, 0]"
-    assert str(b) == "[x[0], x[1], x[2], x[3]]"
+    assert str(a) == "farray([x[4], x[5], x[6], x[7], 0, 0, 0, 0])"
+    assert str(b) == "farray([x[0], x[1], x[2], x[3]])"
     a, b = X.rsh(4, Y)
-    assert str(a) == "[x[4], x[5], x[6], x[7], y[0], y[1], y[2], y[3]]"
-    assert str(b) == "[x[0], x[1], x[2], x[3]]"
+    assert str(a) == "farray([x[4], x[5], x[6], x[7], y[0], y[1], y[2], y[3]])"
+    assert str(b) == "farray([x[0], x[1], x[2], x[3]])"
     a, b = X.rsh(0)
-    assert str(a) == "[x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]]"
+    assert str(a) == "farray([x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]])"
     assert not b
     assert_raises(TypeError, X.__rshift__, "foo")
-    assert str(X >> 4) == "[x[4], x[5], x[6], x[7], 0, 0, 0, 0]"
-    assert str(X >> (4, Y)) == "[x[4], x[5], x[6], x[7], y[0], y[1], y[2], y[3]]"
+    assert str(X >> 4) == "farray([x[4], x[5], x[6], x[7], 0, 0, 0, 0])"
+    assert str(X >> (4, Y)) == "farray([x[4], x[5], x[6], x[7], y[0], y[1], y[2], y[3]])"
 
     # arithmetic right shift
     assert_raises(ValueError, X.arsh, -1)
     assert_raises(ValueError, X.arsh, 9)
     a, b = X.arsh(4)
-    assert str(a) == "[x[4], x[5], x[6], x[7], x[7], x[7], x[7], x[7]]"
-    assert str(b) == "[x[0], x[1], x[2], x[3]]"
+    assert str(a) == "farray([x[4], x[5], x[6], x[7], x[7], x[7], x[7], x[7]])"
+    assert str(b) == "farray([x[0], x[1], x[2], x[3]])"
     a, b = X.arsh(0)
-    assert str(a) == "[x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]]"
+    assert str(a) == "farray([x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]])"
     assert not b
 
 def test_decode():
