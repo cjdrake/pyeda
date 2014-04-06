49b5061671d657711e0c937d83b65db2f38c8aaf
Chris Drake <cjdrake@gmail.com>
2014-04-06 12:28:36 -0700

    Change args_str to a protected _joinargs method

--- pyeda/boolalg/expr.py
+++ pyeda/boolalg/expr.py
@@ -1354,7 +1354,7 @@ class _ArgumentContainer(Expression):
         return (self.ASTOP, ) + tuple(arg.to_ast() for arg in self.args)
 
     # Specific to _ArgumentContainer
-    def args_str(self, sep):
+    def _joinargs(self, sep):
         """Return arguments as a string, joined by a separator."""
         return sep.join(str(arg) for arg in sorted(self.args))
 
@@ -1579,7 +1579,7 @@ class ExprOr(ExprOrAnd):
     DOMINATOR = EXPRONE
 
     def __str__(self):
-        return "Or(" + self.args_str(", ") + ")"
+        return "Or(" + self._joinargs(", ") + ")"
 
     def to_unicode(self):
         parts = list()
@@ -1693,7 +1693,7 @@ class ExprAnd(ExprOrAnd):
     DOMINATOR = EXPRZERO
 
     def __str__(self):
-        return "And(" + self.args_str(", ") + ")"
+        return "And(" + self._joinargs(", ") + ")"
 
     def __enter__(self):
         for arg in self.args:
@@ -1844,7 +1844,7 @@ class ExprNor(ExprNorNand):
             return super(ExprNor, cls).__new__(cls)
 
     def __str__(self):
-        return "Nor(" + self.args_str(", ") + ")"
+        return "Nor(" + self._joinargs(", ") + ")"
 
     def to_unicode(self):
         return ExprNot.SYMBOL + "(" + ExprOr(*self.args).to_unicode() + ")"
@@ -1916,7 +1916,7 @@ class ExprNand(ExprNorNand):
             return super(ExprNand, cls).__new__(cls)
 
     def __str__(self):
-        return "Nand(" + self.args_str(", ") + ")"
+        return "Nand(" + self._joinargs(", ") + ")"
 
     def to_unicode(self):
         return ExprNot.SYMBOL + "(" + ExprAnd(*self.args).to_unicode() + ")"
@@ -2062,7 +2062,7 @@ class ExprXor(ExprExclusive):
             return super(ExprXor, cls).__new__(cls)
 
     def __str__(self):
-        return "Xor(" + self.args_str(", ") + ")"
+        return "Xor(" + self._joinargs(", ") + ")"
 
     def to_unicode(self):
         parts = list()
@@ -2112,7 +2112,7 @@ class ExprXnor(ExprExclusive):
             return super(ExprXnor, cls).__new__(cls)
 
     def __str__(self):
-        return "Xnor(" + self.args_str(", ") + ")"
+        return "Xnor(" + self._joinargs(", ") + ")"
 
     def to_unicode(self):
         return ExprNot.SYMBOL + "(" + ExprXor(*self.args).to_unicode() + ")"
@@ -2155,7 +2155,7 @@ class ExprEqual(ExprEqualBase):
             return super(ExprEqual, cls).__new__(cls)
 
     def __str__(self):
-        return "Equal(" + self.args_str(", ") + ")"
+        return "Equal(" + self._joinargs(", ") + ")"
 
     def to_unicode(self):
         parts = list()
@@ -2246,7 +2246,7 @@ class ExprUnequal(ExprEqualBase):
             return super(ExprUnequal, cls).__new__(cls)
 
     def __str__(self):
-        return "Unequal(" + self.args_str(", ") + ")"
+        return "Unequal(" + self._joinargs(", ") + ")"
 
     def to_unicode(self):
         return ExprNot.SYMBOL + "(" + ExprEqual(*self.args).to_unicode() + ")"
