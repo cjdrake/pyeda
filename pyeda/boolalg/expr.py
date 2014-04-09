84213bb9f47bf70389f9b884ac897177673e9f1f
Chris Drake <cjdrake@gmail.com>
2014-04-08 23:03:36 -0700

    Lint cleanup

--- pyeda/boolalg/expr.py
+++ pyeda/boolalg/expr.py
@@ -717,6 +717,11 @@ class Expression(boolfunc.Function):
         # pylint: disable=R0201
         return False
 
+    @cached_property
+    def _cover(self):
+        """Return the cover representation."""
+        raise NotImplementedError()
+
     @property
     def cover(self):
         """Return the DNF expression as a cover of cubes."""
@@ -1049,7 +1054,6 @@ class ExprLiteral(Expression, sat.DPLLInterface):
 
     @cached_property
     def _cover(self):
-        """Return the cover representation."""
         return {self._cube}
 
     def _absorb(self):
@@ -1641,7 +1645,6 @@ class ExprOr(ExprOrAnd):
     # FlattenedExpression
     @cached_property
     def _cover(self):
-        """Return the cover representation."""
         return {arg._cube for arg in self.args}
 
     @property
@@ -1788,7 +1791,6 @@ class ExprAnd(ExprOrAnd):
 
     @cached_property
     def _cover(self):
-        """Return the cover representation."""
         return {self._cube}
 
     @property
