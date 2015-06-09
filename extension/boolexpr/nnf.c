86f0067f49ac6b9ff4f63b55fc05a17791635146
Chris Drake <cjdrake@gmail.com>
2015-06-08 23:02:30 -0700

    Clean up NNF module

--- extension/boolexpr/nnf.c
+++ extension/boolexpr/nnf.c
@@ -20,6 +20,7 @@ struct BoolExpr * _simplify(struct BoolExpr *ex);
 
 /* util.c */
 struct BoolExpr * _op_transform(struct BoolExpr *op, struct BoolExpr * (*fn)(struct BoolExpr *));
+void _free_xs(int n, struct BoolExpr **xs);
 void _mark_flags(struct BoolExpr *ex, BoolExprFlags f);
 
 
@@ -47,42 +48,40 @@ _xor_nnfify_conj(struct BoolExpr *op)
         CHECK_NULL_2(or_xn0_xn1, OrN(2, xn0, xn1), xn0, xn1);
         BoolExpr_DecRef(xn0);
         BoolExpr_DecRef(xn1);
-
         CHECK_NULL_1(or_x0_x1, OrN(2, op->data.xs->items[0], op->data.xs->items[1]), or_xn0_xn1);
-        CHECK_NULL_2(y, AndN(2, or_xn0_xn1, or_x0_x1), or_xn0_xn1, or_x0_x1);
+        y = AndN(2, or_xn0_xn1, or_x0_x1);
         BoolExpr_DecRef(or_xn0_xn1);
         BoolExpr_DecRef(or_x0_x1);
+
+        return y;
+    }
+
+    size_t mid = op->data.xs->length / 2;
+    size_t n0 = mid;
+    size_t n1 = op->data.xs->length - mid;
+    struct BoolExpr **items0 = op->data.xs->items;
+    struct BoolExpr **items1 = op->data.xs->items + mid;
+    struct BoolExpr *xs[2];
+    struct BoolExpr *temp;
+
+    /* Xor(a, b, c, d) <=> Xor(Xor(a, b), Xor(c, d)) */
+    if (n0 == 1) {
+        xs[0] = BoolExpr_IncRef(items0[0]);
     }
     else {
-        size_t mid = op->data.xs->length / 2;
-        size_t n0 = mid;
-        size_t n1 = op->data.xs->length - mid;
-        struct BoolExpr **items0 = op->data.xs->items;
-        struct BoolExpr **items1 = op->data.xs->items + mid;
-        struct BoolExpr *xs[2];
-        struct BoolExpr *temp;
-
-        /* Xor(a, b, c, d) <=> Xor(Xor(a, b), Xor(c, d)) */
-        if (n0 == 1) {
-            xs[0] = BoolExpr_IncRef(items0[0]);
-        }
-        else {
-            CHECK_NULL(temp, Xor(n0, items0));
-            CHECK_NULL_1(xs[0], _xor_nnfify(temp), temp);
-            BoolExpr_DecRef(temp);
-        }
-
-        CHECK_NULL_1(temp, Xor(n1, items1), xs[0]);
-        CHECK_NULL_2(xs[1], _xor_nnfify(temp), xs[0], temp);
-        BoolExpr_DecRef(temp);
-
-        CHECK_NULL_2(temp, Xor(2, xs), xs[0], xs[1]);
-        BoolExpr_DecRef(xs[0]);
-        BoolExpr_DecRef(xs[1]);
-
-        CHECK_NULL_1(y, _xor_nnfify(temp), temp);
+        CHECK_NULL(temp, Xor(n0, items0));
+        CHECK_NULL_1(xs[0], _xor_nnfify(temp), temp);
         BoolExpr_DecRef(temp);
     }
+
+    CHECK_NULL_1(temp, Xor(n1, items1), xs[0]);
+    CHECK_NULL_2(xs[1], _xor_nnfify(temp), xs[0], temp);
+    BoolExpr_DecRef(temp);
+    CHECK_NULL_2(temp, Xor(2, xs), xs[0], xs[1]);
+    BoolExpr_DecRef(xs[0]);
+    BoolExpr_DecRef(xs[1]);
+    y = _xor_nnfify(temp);
+    BoolExpr_DecRef(temp);
 
     return y;
 }
@@ -102,44 +101,41 @@ _xor_nnfify_disj(struct BoolExpr *op)
         CHECK_NULL(xn0, Not(op->data.xs->items[0]));
         CHECK_NULL_1(and_xn0_x1, AndN(2, xn0, op->data.xs->items[1]), xn0);
         BoolExpr_DecRef(xn0);
-
         CHECK_NULL_1(xn1, Not(op->data.xs->items[1]), and_xn0_x1);
         CHECK_NULL_2(and_x0_xn1, AndN(2, op->data.xs->items[0], xn1), xn1, and_xn0_x1);
         BoolExpr_DecRef(xn1);
-
-        CHECK_NULL_2(y, OrN(2, and_xn0_x1, and_x0_xn1), and_xn0_x1, and_x0_xn1);
+        y = OrN(2, and_xn0_x1, and_x0_xn1);
         BoolExpr_DecRef(and_xn0_x1);
         BoolExpr_DecRef(and_x0_xn1);
+
+        return y;
+    }
+
+    size_t mid = op->data.xs->length / 2;
+    size_t n0 = mid;
+    size_t n1 = op->data.xs->length - mid;
+    struct BoolExpr **items0 = op->data.xs->items;
+    struct BoolExpr **items1 = op->data.xs->items + mid;
+    struct BoolExpr *xs[2];
+    struct BoolExpr *temp;
+
+    if (n0 == 1) {
+        xs[0] = BoolExpr_IncRef(items0[0]);
     }
     else {
-        size_t mid = op->data.xs->length / 2;
-        size_t n0 = mid;
-        size_t n1 = op->data.xs->length - mid;
-        struct BoolExpr **items0 = op->data.xs->items;
-        struct BoolExpr **items1 = op->data.xs->items + mid;
-        struct BoolExpr *xs[2];
-        struct BoolExpr *temp;
-
-        if (n0 == 1) {
-            xs[0] = BoolExpr_IncRef(items0[0]);
-        }
-        else {
-            CHECK_NULL(temp, Xor(n0, items0));
-            CHECK_NULL_1(xs[0], _xor_nnfify(temp), temp);
-            BoolExpr_DecRef(temp);
-        }
-
-        CHECK_NULL_1(temp, Xor(n1, items1), xs[0]);
-        CHECK_NULL_2(xs[1], _xor_nnfify(temp), xs[0], temp);
-        BoolExpr_DecRef(temp);
-
-        CHECK_NULL_2(temp, Xor(2, xs), xs[0], xs[1]);
-        BoolExpr_DecRef(xs[0]);
-        BoolExpr_DecRef(xs[1]);
-
-        CHECK_NULL_1(y, _xor_nnfify(temp), temp);
+        CHECK_NULL(temp, Xor(n0, items0));
+        CHECK_NULL_1(xs[0], _xor_nnfify(temp), temp);
         BoolExpr_DecRef(temp);
     }
+
+    CHECK_NULL_1(temp, Xor(n1, items1), xs[0]);
+    CHECK_NULL_2(xs[1], _xor_nnfify(temp), xs[0], temp);
+    BoolExpr_DecRef(temp);
+    CHECK_NULL_2(temp, Xor(2, xs), xs[0], xs[1]);
+    BoolExpr_DecRef(xs[0]);
+    BoolExpr_DecRef(xs[1]);
+    y = _xor_nnfify(temp);
+    BoolExpr_DecRef(temp);
 
     return y;
 }
@@ -172,20 +168,33 @@ _eq_nnfify(struct BoolExpr *op)
 {
     int length = op->data.xs->length;
     struct BoolExpr **xs = op->data.xs->items;
-    struct BoolExpr *xns[length];
+    struct BoolExpr **xns;
     struct BoolExpr *all0, *all1;
     struct BoolExpr *y;
 
+    xns = malloc(length * sizeof(struct BoolExpr *));
+    if (xns == NULL)
+        return NULL;
+
     /* Equal(x0, x1, x2) <=> ~x0 & ~x1 & ~x2 | x0 & x1 & x2 */
-    for (size_t i = 0; i < length; ++i)
-        CHECK_NULL_N(xns[i], Not(xs[i]), i, xns);
-    CHECK_NULL_N(all0, And(length, xns), length, xns);
-    for (size_t i = 0; i < length; ++i)
-        BoolExpr_DecRef(xns[i]);
+    for (size_t i = 0; i < length; ++i) {
+        xns[i] = Not(xs[i]);
+        if (xns[i] == NULL) {
+            _free_xs(i, xns);
+            return NULL;
+        }
+    }
+
+    all0 = And(length, xns);
+    if (all0 == NULL) {
+        _free_xs(length, xns);
+        return NULL;
+    }
+
+    _free_xs(length, xns);
 
     CHECK_NULL_1(all1, And(length, xs), all0);
-
-    CHECK_NULL_2(y, OrN(2, all0, all1), all0, all1);
+    y = OrN(2, all0, all1);
     BoolExpr_DecRef(all0);
     BoolExpr_DecRef(all1);
 
@@ -206,7 +215,7 @@ _impl_nnfify(struct BoolExpr *op)
 
     /* p => q <=> ~p | q */
     CHECK_NULL(pn, Not(p));
-    CHECK_NULL_1(y, OrN(2, pn, q), pn);
+    y = OrN(2, pn, q);
     BoolExpr_DecRef(pn);
 
     return y;
@@ -230,10 +239,8 @@ _ite_nnfify_conj(struct BoolExpr *op)
     CHECK_NULL(sn, Not(s));
     CHECK_NULL_1(or_sn_d1, OrN(2, sn, d1), sn);
     BoolExpr_DecRef(sn);
-
     CHECK_NULL_1(or_s_d0, OrN(2, s, d0), or_sn_d1);
-
-    CHECK_NULL_2(y, AndN(2, or_sn_d1, or_s_d0), or_sn_d1, or_s_d0);
+    y = AndN(2, or_sn_d1, or_s_d0);
     BoolExpr_DecRef(or_sn_d1);
     BoolExpr_DecRef(or_s_d0);
 
@@ -256,12 +263,10 @@ _ite_nnfify_disj(struct BoolExpr *op)
 
     /* s ? d1 : d0 <=> s & d1 | ~s & d0 */
     CHECK_NULL(and_s_d1, AndN(2, s, d1));
-
     CHECK_NULL(sn, Not(s));
     CHECK_NULL_1(and_sn_d0, AndN(2, sn, d0), sn);
     BoolExpr_DecRef(sn);
-
-    CHECK_NULL_2(y, OrN(2, and_s_d1, and_sn_d0), and_s_d1, and_sn_d0);
+    y = OrN(2, and_s_d1, and_sn_d0);
     BoolExpr_DecRef(and_s_d1);
     BoolExpr_DecRef(and_sn_d0);
 
@@ -309,18 +314,15 @@ static struct BoolExpr * (*_op_nnfify[16])(struct BoolExpr *op) = {
 struct BoolExpr *
 _nnfify(struct BoolExpr *ex)
 {
+    if (IS_NNF(ex))
+        return BoolExpr_IncRef(ex);
+
+    struct BoolExpr *temp;
     struct BoolExpr *y;
 
-    if (IS_NNF(ex)) {
-        y = BoolExpr_IncRef(ex);
-    }
-    else {
-        struct BoolExpr *temp;
-
-        CHECK_NULL(temp, _op_transform(ex, _nnfify));
-        CHECK_NULL_1(y, _op_nnfify[temp->kind](temp), temp);
-        BoolExpr_DecRef(temp);
-    }
+    CHECK_NULL(temp, _op_transform(ex, _nnfify));
+    y = _op_nnfify[temp->kind](temp);
+    BoolExpr_DecRef(temp);
 
     return y;
 }
