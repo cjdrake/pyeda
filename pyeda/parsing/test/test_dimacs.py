60bfdc081afd4833d4982c65590f520f452ea1c2
Chris Drake <cjdrake@gmail.com>
2022-12-10 20:41:57 -0500

    Lint cleanup: test_dimacs.py

--- pyeda/parsing/test/test_dimacs.py
+++ pyeda/parsing/test/test_dimacs.py
@@ -2,6 +2,7 @@
 Test DIMACS load/dump methods
 """
 
+
 from nose.tools import assert_raises
 
 from pyeda.parsing.dimacs import Error, parse_cnf, parse_sat
@@ -23,16 +24,18 @@ def test_cnf_errors():
     # incomplete clause
     assert_raises(Error, parse_cnf, "p cnf 1 1\n1")
 
+
 def test_parse_cnf():
     # Empty formula corner cases
-    assert parse_cnf("p cnf 0 0\n") == ('and', )
-    assert parse_cnf("p cnf 1 0\n") == ('and', )
+    assert parse_cnf("p cnf 0 0\n") == ("and", )
+    assert parse_cnf("p cnf 1 0\n") == ("and", )
 
     # Empty clause corner cases
-    assert parse_cnf("p cnf 0 1\n0") == ('and', ('or', ),)
-    assert parse_cnf("p cnf 1 2\n0 0") == ('and', ('or', ),('or', ))
+    assert parse_cnf("p cnf 0 1\n0") == ("and", ("or", ),)
+    assert parse_cnf("p cnf 1 2\n0 0") == ("and", ("or", ),("or", ))
 
-    assert parse_cnf("p cnf 2 2\n-1 2 0 1 -2 0") == ('and', ('or', ('not', ('var', ('x', ), (1, ))), ('var', ('x', ), (2, ))), ('or', ('var', ('x', ), (1, )), ('not', ('var', ('x', ), (2, )))))
+    assert parse_cnf("p cnf 2 2\n-1 2 0 1 -2 0") == ("and", ("or", ("not", ("var", ("x", ), (1, ))), ("var", ("x", ), (2, ))), ("or", ("var", ("x", ), (1, )), ("not", ("var", ("x", ), (2, )))))
+
 
 def test_sat_errors():
     assert_raises(Error, parse_sat, "#a")
@@ -43,10 +46,10 @@ def test_sat_errors():
     assert_raises(Error, parse_sat, "p sat 2\n-(0)")
     assert_raises(Error, parse_sat, "p sat 2\n-(3)")
 
-def test_parse_sat():
-    assert parse_sat("p sat 1\n(-1)") == ('not', ('var', ('x', ), (1, )))
-    assert parse_sat("p sat 2\n-(+(*(-1 2) *(1 -2)))") == ('not', ('or', ('and', ('not', ('var', ('x', ), (1, ))), ('var', ('x', ), (2, ))), ('and', ('var', ('x', ), (1, )), ('not', ('var', ('x', ), (2, ))))))
-    assert parse_sat("p satx 2\nxor(-1 2)") == ('xor', ('not', ('var', ('x', ), (1, ))), ('var', ('x', ), (2, )))
-    assert parse_sat("p sate 2\n=(-1 2)") == ('equal', ('not', ('var', ('x', ), (1, ))), ('var', ('x', ), (2, )))
-    assert parse_sat("p satex 2\n+(xor(-1 2) =(1 -2))") == ('or', ('xor', ('not', ('var', ('x', ), (1, ))), ('var', ('x', ), (2, ))), ('equal', ('var', ('x', ), (1, )), ('not', ('var', ('x', ), (2, )))))
 
+def test_parse_sat():
+    assert parse_sat("p sat 1\n(-1)") == ("not", ("var", ("x", ), (1, )))
+    assert parse_sat("p sat 2\n-(+(*(-1 2) *(1 -2)))") == ("not", ("or", ("and", ("not", ("var", ("x", ), (1, ))), ("var", ("x", ), (2, ))), ("and", ("var", ("x", ), (1, )), ("not", ("var", ("x", ), (2, ))))))
+    assert parse_sat("p satx 2\nxor(-1 2)") == ("xor", ("not", ("var", ("x", ), (1, ))), ("var", ("x", ), (2, )))
+    assert parse_sat("p sate 2\n=(-1 2)") == ("equal", ("not", ("var", ("x", ), (1, ))), ("var", ("x", ), (2, )))
+    assert parse_sat("p satex 2\n+(xor(-1 2) =(1 -2))") == ("or", ("xor", ("not", ("var", ("x", ), (1, ))), ("var", ("x", ), (2, ))), ("equal", ("var", ("x", ), (1, )), ("not", ("var", ("x", ), (2, )))))
