dd9281fca0c3e0ebdc9acfaaaee2195f75d1b221
Chris Drake <cjdrake@gmail.com>
2022-12-10 20:51:35 -0500

    Lint cleanup: test_sudoku.py

--- pyeda/logic/test/test_sudoku.py
+++ pyeda/logic/test/test_sudoku.py
@@ -2,8 +2,8 @@
 Test logic functions for Sudoku
 """
 
+
 import os
-import random
 
 from pyeda.logic.sudoku import SudokuSolver
 
@@ -11,14 +11,14 @@ from pyeda.logic.sudoku import SudokuSolver
 SOLVER = SudokuSolver()
 
 cwd, _ = os.path.split(__file__)
-top95_txt = os.path.join(cwd, 'top95.txt')
-top95_solns_txt = os.path.join(cwd, 'top95_solns.txt')
-with open(top95_txt) as fin:
+top95_txt = os.path.join(cwd, "top95.txt")
+top95_solns_txt = os.path.join(cwd, "top95_solns.txt")
+with open(top95_txt, encoding="utf-8") as fin:
     GRIDS = [line.strip() for line in fin]
-with open(top95_solns_txt) as fin:
+with open(top95_solns_txt, encoding="utf-8") as fin:
     SOLNS = [line.strip() for line in fin]
+
 
 def test_sudoku():
     for i, grid in enumerate(GRIDS):
         assert SOLVER.display_solve(grid) == SOLNS[i]
-
